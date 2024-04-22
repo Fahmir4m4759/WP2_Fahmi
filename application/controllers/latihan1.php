d8690ea80036210ed8715d3e8524cc8703d7ecb3
fahmir4m4759 <15220675@bsi.ac.id>
2024-04-22 20:03:51 +0700

    pertemuan4 dan tuags 3

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -1,12 +1,12 @@
 <?php
-class latian1 extends CI_Controller
+class Latihan1 extends CI_Controller
 {
  public function index()
  {
-     echo "Selamat Datang..selamat belajar web programming";
+ echo "Selamat Datang.. selamat belajar Web Programming";
+ //$this->load->view('view-latihan1');
  }
-}
-public function penjumlahan($n1, $n2)
+ public function penjumlahan($n1, $n2)
  {
  $this->load->model('Model_latihan1');
  $data['nilai1'] = $n1;
@@ -14,3 +14,4 @@ public function penjumlahan($n1, $n2)
  $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
  $this->load->view('view-latihan1', $data);
  }
+}
--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -1,13 +1,14 @@
 <?php
-class Model_latihan1 extends CI-MODEL
+class Model_latihan1 extends CI_Model
 {
-    //membuat variabel unutk menampung nilai
-    public $nilai1, $nilai2, $hasil;
-
-    //method penjumlahan
-    public function jumlah($nil1 = null, $nil2 = null)
+ //membuat variable untuk menampung nilai
+ public $nilai1, $nilai2, $hasil;
+ //method penjumlahan
+ public function jumlah($n1 = null, $n2 = null)
+ {
+ $this->nilai1 = $nil1;
+ $this->nilai2 = $nil2;
+ $this->hasil = $this->nilai1 + $this->nilai2;
+ return $this->hasil;
+ }
 }
-$this->nilai1 = $nilai1;
-$this->nilai2 = $nilai2;
-$this->hasil = $this->nilai1 + $this->nilai2;
-return $this->hasil;
