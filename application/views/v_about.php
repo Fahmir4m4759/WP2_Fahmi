6a6b392db3cefbdfcfa8f7643468eda038bb9d4a
Fahmir4m4759 <15220675@bsi.ac.id>
2024-04-24 17:12:55 +0700

    pertemuan 5 dan tugas 4

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Herman</li>
+        <li>Nama Belakang : Firmansyah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Purnajaya 1 jalur 2 No.28</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="circle">
+        <li>Teluk Pakedai</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
