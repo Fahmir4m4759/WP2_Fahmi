2fe563665f4e44e0642a457f47fffed1fd17ee9d
Fahmir4m4759 <15220675@bsi.ac.id>
2024-05-27 14:50:51 +0700

    pertemuan 9 dan tugas 7

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya herman firmansyah <br>
+        echo "Nama saya Fahmi Rama Adrian <br>
         saya tinggal di purnajaya 1 jalur 2 no.28, <br>
         saya adalah seorang mahasiswa ubsi semester 4,<br>
          dan ini adala tugas pertama makul WP2";
